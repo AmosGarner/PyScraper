8be9aeb8e0d4b7204225af7130bdd4d4b41aaf06
Amos Garner <aagarner.7@gmail.com>
2017-04-25 17:20:55 -0400

    Implement check for search type and counter case

--- PyTriSearch.py
+++ PyTriSearch.py
@@ -1,15 +1,20 @@
-import sys, requests, pyperclip
+import sys, requests, pyperclip, argparse
 import webbrowser as browser
 
 googleUrl = 'https://www.google.com/search?q='
 connecter = '&'
-imgSearchType = 'tbm=isch'
+imgSearchFlag = 'tbm=isch'
 
 def main():
-    if len(sys.argv) > 1:
-        return False
+    parser = parser = argparse.ArgumentParser(description="Runs PyTriSearch googling utility.")
+    parser.add_argument('--type', dest='search_type', required=False)
+    parser.add_argument('--search', dest='keywords', required=True)
+    arguments = parser.parse_args()
+
+    if arguments.search_type == 'image' or arguments.search_type == 'i':
+        browser.open(googleUrl + 'cat' + connecter + imgSearchFlag)
     else:
-        browser.open(googleUrl + 'cat' + connecter + imgSearchType)
+        browser.open(googleUrl + 'cat')
 
 if __name__ == '__main__':
     main()
