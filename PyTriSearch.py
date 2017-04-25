9e72fa740e85fe016013c7a1b73f3e9553b42c9c
Amos Garner <aagarner.7@gmail.com>
2017-04-25 18:07:13 -0400

    [Incremental] implementation of display top n results feature

--- PyTriSearch.py
+++ PyTriSearch.py
@@ -18,23 +18,29 @@ def main():
     parser = parser = argparse.ArgumentParser(description="Runs PyTriSearch googling utility.")
     parser.add_argument('--type', dest='search_type', required=False)
     parser.add_argument('--search', dest='keywords', required=True)
+    parser.add_argument('--total', dest='total_results', required=False)
     arguments = parser.parse_args()
 
     if arguments.search_type == 'image' or arguments.search_type == 'i':
         browser.open(googleUrl + generateURLReadyKeywords(arguments.keywords) + connecter + imgSearchFlag)
     else:
+        if arguments.total_results is None:
+            arguments.total_results = 5
+
         browser.open(googleUrl + generateURLReadyKeywords(arguments.keywords))
         res = requests.get(googleUrl + generateURLReadyKeywords(arguments.keywords))
         res.raise_for_status()
         searchResultsHTML = bs4.BeautifulSoup(res.text, 'html.parser')
         results = searchResultsHTML.select('.r a')
 
-        if(len(results)  >= 5):
-            results = results[:5]
+        if len(results)  >= arguments.total_results:
+            results = results[:arguments.total_results]
+        else if len()
+
 
         print '%s  returned, here are the top %s!' % (searchResultsHTML.select('#resultStats')[0].text, len(results))
         for result in results:
-            print(result.href)
+            browser.open(result.get('href').strip('/url?q='))
 
 if __name__ == '__main__':
     main()
