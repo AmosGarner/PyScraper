d381a8222a938b54a9722dc9f929562933651d0e
Amos Garner <aagarner.7@gmail.com>
2017-04-25 15:59:24 -0400

    Implement getWebReadyAddress functionality

--- PyMap.py
+++ PyMap.py
@@ -1,6 +1,9 @@
 import sys, pyperclip
 import webbrowser as browser
 
+mapsURL = 'https://www.google.com/maps/place/'
+addr = '1046+Circle+Dr,+Seymour,+TN+37865'
+
 def getAddressFromArguments(arguments):
     address = ''
     for argument in arguments:
@@ -10,14 +13,24 @@ def getAddressFromArguments(arguments):
 def getAddressFromClipboard():
     return pyperclip.paste()
 
+def getWebReadyAddress(address):
+    charArray = list(address)
+    address = ''
+    for character in charArray:
+        if(character == ' '):
+            address += '+'
+        else:
+            address += character
+    return address
+
 def main():
     address = ''
     if len(sys.argv) > 1:
         address = getAddressFromArguments(sys.argv[1:])
     else:
         address = getAddressFromClipboard()
-
-    print(address)
+    address = getWebReadyAddress(address)
+    browser.open(mapsURL + address)
 
 if __name__ == '__main__':
     main()
