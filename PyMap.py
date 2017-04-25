ab0e5d03c8d5706703d2cee1983154d72ebab992
Amos Garner <aagarner.7@gmail.com>
2017-04-25 15:37:38 -0400

    Implement getAddress from command line function

--- PyMap.py
+++ PyMap.py
@@ -1,6 +1,13 @@
+import sys
 import webbrowser as browser
 
+def getAddressFromArguments(arguments):
+    return ''.join(arguments(1:])
+
 def main():
+    if len(sys.argv) > 1:
+        getAddressFromArguments(sys.argv[1:])
+
     return False
 
 if __name__ == '__main__':
