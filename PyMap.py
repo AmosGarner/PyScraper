e92a56c85753cf4f5021414db5fb7a35c564eac3
Amos Garner <aagarner.7@gmail.com>
2017-04-25 15:44:29 -0400

    Implement proper address from arguments func, get address from clipboard, and refactor syntax erros

--- PyMap.py
+++ PyMap.py
@@ -1,14 +1,23 @@
-import sys
+import sys, pyperclip
 import webbrowser as browser
 
 def getAddressFromArguments(arguments):
-    return ''.join(arguments(1:])
+    address = ''
+    for argument in arguments:
+        address += ' ' + argument
+    return address
+
+def getAddressFromClipboard():
+    return pyperclip.paste()
 
 def main():
+    address = ''
     if len(sys.argv) > 1:
-        getAddressFromArguments(sys.argv[1:])
+        address = getAddressFromArguments(sys.argv[1:])
+    else:
+        address = getAddressFromClipboard()
 
-    return False
+    print(address)
 
 if __name__ == '__main__':
     main()
