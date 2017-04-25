6b914f4365005344473443c3e9845ee54fad7e19
Amos Garner <aagarner.7@gmail.com>
2017-04-25 17:07:49 -0400

    Implement check for arguments and image search cats

--- PyTriSearch.py
+++ PyTriSearch.py
@@ -1,8 +1,15 @@
-import sys, requests
+import sys, requests, pyperclip
 import webbrowser as browser
 
+googleUrl = 'https://www.google.com/search?q='
+connecter = '&'
+imgSearchType = 'tbm=isch'
+
 def main():
-    return False
+    if len(sys.argv) > 1:
+        return False
+    else:
+        browser.open(googleUrl + 'cat' + connecter + imgSearchType)
 
 if __name__ == '__main__':
     main()
